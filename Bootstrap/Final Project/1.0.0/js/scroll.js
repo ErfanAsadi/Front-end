3bd5dabea70564df2e8a679659646ac662a9ee9c
erfan <erfanasadi.ce@gmail.com>
2020-08-20 18:40:40 +0430

    First rep commit

---
+++ Bootstrap/Final Project/1.0.0/js/scroll.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  // add padding top to show content behind navbar
+  // $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+
+  // detect scroll top or down
+  if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+      scroll_top = $(this).scrollTop();
+      if (scroll_top < last_scroll_top) {
+        $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+      } else {
+        $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+      }
+      last_scroll_top = scroll_top;
+    });
+  }
+
+  $("a[href^='#']").click(function(e) {
+    if (this.className != "nav-item") {
+      e.preventDefault();
+      var position = $($(this).attr("href")).offset().top;
+
+      $("body, html").animate({
+        scrollTop: position
+      }, 1200);
+    }
+
+  });
+});
